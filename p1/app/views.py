5ab2d98c330126eda9dfeecabb900e8d894e3d64
Prajwal B S <prajwaljhon@gmail.com>
2024-08-15 20:56:33 +0530

    A Function Greeting Based On Time is created

--- p1/app/views.py
+++ p1/app/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse as hr
 from time import gmtime, strftime
+import time
 # Create your views here.
 def wish(request):
     s = strftime("%a, %d %b %Y %H:%M:%S", 
-             gmtime(1627987508.6496193))
-    
-    return hr("Evening")
+             gmtime(time.time()))
+    obj = time.strptime(s, "%a, %d %b %Y %H:%M:%S")
+    if(obj.tm_hour+5)<12:
+        return hr("Good Morning")
+    elif(obj.tm_hour+5)<18:
+        return hr("Good Afternoon")
+    else:
+        return hr("Good Evening")
