29eb8841520d9b1013fcdfb35e03bdf6d17e518e
Prajwal B S <prajwaljhon@gmail.com>
2024-08-29 21:10:29 +0530

    Url Navigation Done

--- jinjaop/app/views.py
+++ jinjaop/app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def cond(re):
     return render(re,'cond.html',{'no':10})
+
+def loop(re):
+    return render(re,'loop.html',{'name':'Amin'})
--- jinjaop/jinjaop/urls.py
+++ jinjaop/jinjaop/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cond/',cond,name='cond')
+    path('cond/',cond,name='cond'),
+    path('loop/',loop,name='loop'),
 ]
