832a92fba869859016b7b09cc03814dccdaa2b83
Prajwal B S <prajwaljhon@gmail.com>
2024-09-22 22:35:28 +0530

    Created A good Ui for Goodemp project

--- goodemp/app/models.py
+++ goodemp/app/models.py
@@ -12,7 +12,7 @@ class Emp(models.Model):
     eno=models.IntegerField(primary_key=True)
     ename=models.CharField(max_length=100)
     job=models.CharField(max_length=100)
-    hiredate=models.DateField(auto_now=True)
+    hiredate=models.DateField(auto_now_add=True)
     sal=models.DecimalField(max_digits=10,decimal_places=2)
     dno=models.ForeignKey(Dept,on_delete=models.CASCADE)
     mgr=models.ForeignKey('self',on_delete=models.SET_NULL,null=True,blank=True)
--- goodemp/app/views.py
+++ goodemp/app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from app.models import *
 # Create your views here.
+def op(re):
+    emp=Emp.objects.all()
+    return render(re,'op.html',{'emp':emp})
