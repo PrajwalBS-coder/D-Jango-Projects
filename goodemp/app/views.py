1e93b60f2f3f9f527e60a5e06cd38525f4715e9f
Prajwal B S <prajwaljhon@gmail.com>
2024-09-27 12:41:08 +0530

    Altered 'views' so that it can work with accepting empty manager no as well as commssion

--- goodemp/app/views.py
+++ goodemp/app/views.py
@@ -15,9 +15,9 @@ def insert(re):
     dno=input('Enter dno')
     mgr=input('Enter mgr')
     com=input('Enter commission')
-    DO=Dept.objects.get(deptno=dno)
+    DO=Dept.objects.get(deptno=dno)#if department no doesnot exist it'll throw error snd stops the execution
     if DO:
-        MO=Emp.objects.get(eno=mgr)
+        MO=Emp.objects.get(eno=mgr)#if Manager no doesnot exist it'll throw error snd stops the execution
         if MO:
             EO=Emp.objects.get_or_create(eno=eno,ename=na,job=job,sal=sal,dno=DO,mgr=MO,comm=com)
             if EO[1]:
@@ -41,18 +41,23 @@ def insert2(re):
     sal=input('Salary')
     dno=input('Enter dno')
     mgr=input('Enter mgr')
-    com=input('Enter commission')
-    DO=Dept.objects.filter(deptno=dno)
+    com=(input('Enter commission'))
+    if(com):
+        com=int(com)
+    else:
+        com=None
+    DO=Dept.objects.filter(deptno=dno)#if department no doesnot exist we can handle ther error by reponsing an HttpResponse to Web Page That'y it's comparitively better than get method
     if DO:
-        MO=Emp.objects.filter(eno=mgr)
-        if MO:
-            EO=Emp.objects.get_or_create(eno=eno,ename=na,job=job,sal=sal,dno=DO[0],mgr=MO[0],comm=com)
-            if EO[1]:
-                return hr('Created Man')
-            else:
-                return hr('Not Created')
+        if(mgr):
+            MO=Emp.objects.filter(eno=int(mgr))
+            if MO:
+                EO=Emp.objects.get_or_create(eno=eno,ename=na,job=job,hiredate=da,sal=sal,dno=DO[0],mgr=MO[0],comm=com)
+                if EO[1]:
+                    return hr('Created Man')
+                else:
+                    return hr('Not Created')
         else:
-            EO=Emp.objects.get_or_create(eno=eno,ename=na,job=job,sal=sal,dno=DO[0],mgr=MO[0],comm=com)
+            EO=Emp.objects.get_or_create(eno=eno,ename=na,job=job,hiredate=da,sal=sal,dno=DO[0],comm=com)
             if EO[1]:
                 return hr("Created BRo")
             else:
