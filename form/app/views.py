a52909ea9810126172ab43ddbf560f001887ac0f
Prajwal B S <prajwaljhon@gmail.com>
2024-10-12 21:24:55 +0530

    Tried Select_releated But not Getting The Required OutPut

--- form/app/views.py
+++ form/app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import *
 from django.db.models import *
-
+from django.http import HttpResponse as hr
 # Create your views here.
 def Insert_Product_cat(re):
     pcid=input("ENter ProductId")
@@ -20,15 +20,20 @@ def Insert_Product(re):
     pid=input("ENter ProductId")
     pcid=input("ENter PProductCategories Id")
     pname=input("ENter ProductCategories Name")
-    price=input("Enter The Source")
+    price=input("Enter The Price")
     manu=input("enter Manufacture")
     date=input("Enter date")
     PCOBJ=ProductCategoryies.objects.filter(pcid=pcid)
     if (PCOBJ):
-        Product.objects.create(pid=pid,pcid=PCOBJ[0],pname=pname,price=price,manufacture=manu,mdate=date)
-        columns=[field.name for field in Product._meta.get_fields()]
-        PCO= Product.objects.all()
-        return render(re,'all.html',{'data':PCO,'columns':columns})
+        if(pid and pcid and pname and price and manu and date):
+            Product.objects.create(pid=pid,pcid=PCOBJ[0],pname=pname,price=price,manufacture=manu,mdate=date)
+            columns=[field.name for field in Product._meta.get_fields()]
+            PCO= Product.objects.all()
+            return render(re,'all.html',{'data':PCO,'columns':columns})
+        else:
+            return hr('Not A Valid Data')
+    else:
+        return hr('Product Catgories Does NOt Exist')
 def Display_Product(re):
     columns=[field.name for field in Product._meta.get_fields()]
     PCO= Product.objects.all()
@@ -38,6 +43,7 @@ def Queries(re):
     columns2=[field.name for field in ProductCategoryies._meta.get_fields()][1::]
     columns=columns1+columns2
     PCO= ProductCategoryies.objects.prefetch_related('product_set')
+    # PCO= Product.objects.select_related('pcid')
     p= Product.objects.all().aggregate(Avg('price'))
     p= Product.objects.all().aggregate(Min('price'))
     p= Product.objects.all().aggregate(Max('price'))
