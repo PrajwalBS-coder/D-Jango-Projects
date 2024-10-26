298d5930e47f447e0ef80354fd3cb5f3c3c2c2ef
Prajwal B S <prajwaljhon@gmail.com>
2024-10-26 11:30:54 +0530

    Created Model(Tabl) StudentData and Inserted data through My Way And It's Working✌😎😁

--- djforms/app/models.py
+++ djforms/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class StudentData(models.Model):
+    name=models.CharField(max_length=100)
+    sid=models.IntegerField()
+    email=models.EmailField()
+    url=models.URLField()
+    address=models.TextField()
+    password=models.CharField(max_length=100)
+    def __str__(self):
+        return (self.name)
--- djforms/app/views.py
+++ djforms/app/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from app.forms import *
 from django.http import HttpResponse as hr
-
+from .models import *
 # Create your views here.
 def stu(re):
     s=StudentForm()
     if re.method=='POST':
         SDATA=StudentForm(re.POST)
         if SDATA.is_valid():
-            return hr(str(SDATA.cleaned_data))
+            s=StudentData.objects.get_or_create(name=re.POST.get('sname'),sid=re.POST.get('id'),email=re.POST.get('email'),url=re.POST.get('url'),address=re.POST.get('address'),password=re.POST.get('password'))
+            if(s):
+                return hr(str(SDATA.cleaned_data))
+            else:
+                hr("NOT ADDED TO DB")
+            # print(re.POST.get('password'))
+            # return hr(SDATA)
         else:
             return hr("WRONG")
     return render(re,'form.html',{'form':s})
