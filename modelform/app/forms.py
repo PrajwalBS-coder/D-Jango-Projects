0cd35501f187d5ea50471a0990578c4e214d30aa
Prajwal B S <prajwaljhon@gmail.com>
2024-10-28 09:28:58 +0530

    Model Forms

---
+++ modelform/app/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import *
+
+class TopicForm(forms.ModelForm):
+    class Meta:
+        model=topic
+        fields='__all__'
+
+class WebpageForm(forms.ModelForm):
+    class Meta:
+        model=webpage
+        fields='__all__'
+
+class AccessForm(forms.ModelForm):
+    class Meta:
+        model=access
+        fields='__all__'
---
+++ modelform/app/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from .forms import *
+from django.http import HttpResponse as hr
+# Create your views here.
+
+
+def InsertTopic(re):
+    if re.method=='POST':
+        dataobj=TopicForm(re.POST)
+        if dataobj.is_valid():
+            dataobj.save()
+            return hr("OK")
+        else:
+            return hr('Gone')
+
+
+    return render(re,'inserttopic.html',{'form':TopicForm()})
+
+def InsertWebpage(re):
+    if re.method=='POST':
+        dataobj=WebpageForm(re.POST)
+        if dataobj.is_valid():
+            dataobj.save()
+            return hr("OK")
+        else:
+            return hr('Gone')
+
+
+    return render(re,'insertwebpage.html',{'form':WebpageForm()})#insertacccess
+
+def InsertAccess(re):
+    if re.method=='POST':
+        dataobj=AccessForm(re.POST)
+        if dataobj.is_valid():
+            dataobj.save()
+            return hr("OK")
+        else:
+            return hr('Gone')
+
+
+    return render(re,'insertwebpage.html',{'form':AccessForm()})#insertacccess
