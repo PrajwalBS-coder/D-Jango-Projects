75b23dcb1108dafa3407b6aa55487e7da9072042
Prajwal B S <prajwaljhon@gmail.com>
2024-10-31 19:01:11 +0530

    Hosting Our Project Using IP Address,if other devices connected to same n/w they can also access the url and able to see the data in that url

--- modelform/modelform/settings.py
+++ modelform/modelform/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u_i-c!35ywpp$x#hw3en*=4w$05%c99qkrn48y=6$(*u(+ir!o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.34.157']
 
 
 # Application definition
